9b9479f4bcee14a6722139f975f39710b16c5dc9
abir10t <aabir.10t@gmail.com>
2020-11-09 17:09:29 +0600

    1st commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white py-3 px-3">
+  <a>TO-DO Maker</a>
+</nav>
+
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
